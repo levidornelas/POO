d94e046228590fcbb11b418472b0b8f810d518b4
Levi <165521979+levidornelas@users.noreply.github.com>
2024-06-13 10:52:22 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -1,14 +1,23 @@
 class ContaBancaria:
-  def __init__(self, numero_conta, titular, saldo):
-      self.numero_conta = numero_conta
-      self.titular = titular
-      self.saldo = saldo
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
 
-  def verificar_saldo(self):
-      return self.saldo
+    def verificar_saldo(self):
+        return self.saldo
 
-  def deposito(self, deposito):
-      self.saldo += deposito
+    def depositar(self, deposito):
+        self.saldo += deposito
 
-  def sacar(self, saque):
-      self.saldo -= saque
+    def sacar(self, saque):
+        self.saldo -= saque
+
+    def transferir(self, conta_destino, valor):
+        if valor > self.saldo:
+            raise ValueError("Você não tem saldo suficiente para essa transferência.")
+            
+        self.sacar(valor)
+        conta_destino.depositar(valor)
+        
+        print(f'Transferência de R${valor:.2f} realizada com sucesso para {conta_destino.titular}.')
+        print(f'Seu saldo atual é: R${self.verificar_saldo():.2f}')
